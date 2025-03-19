736ac57e7b23f04e8bae3bc4e2c131cf5feb166c
N1ler4 <bahtiyorov.nozim@gmail.com>
2025-03-19 20:48:35 +0500

    MUSO AQQILI

--- index.py
+++ index.py
@@ -1,1 +1,22 @@
-print("123")
+class ValidEmail:
+    def __init__(self):
+        self._emails = {}
+
+    def __get__(self, instance, owner):
+        return self._emails.get(instance, "Email not set")
+
+    def __set__(self, instance, value):
+        if "@" not in value or "." not in value:
+            raise ValueError("Invalid email address")
+        self._emails[instance] = value
+
+
+class User:
+    email = ValidEmail()
+
+
+user = User()
+user.email = "test@example.com"
+print(user.email)  
+
+user.email = "invalid-email"  
